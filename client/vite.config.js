baed35f3d4d9f035ed54a017cb0800296d579689
Abdulhakim Amonov <abdulhakimamonov@MacBook-Air-Abdulhakim.local>
2026-04-20 15:44:28 +0500

    .

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,10 +4,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3001,
     proxy: {
-      '/api': 'http://localhost:5000',
-      '/uploads': 'http://localhost:5000',
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+      '/uploads': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
     }
   }
 })
